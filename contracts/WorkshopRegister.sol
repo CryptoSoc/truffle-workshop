eb809fd1536003ffd05499d81549023ecec7842f
Thomas Tumiel <ttumiel@gmail.com>
2018-10-11 21:42:54 +0200

    register smart contract

---
+++ contracts/WorkshopRegister.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.24;
+// Solidity version 0.4.25 is the latest and greatest and there are probably
+// some breaking changes that I'm unaware of. If you happen to spot any, let
+// me know via a github issue.
+
+// I usually write the contract in remix and test and debug there,
+// then copy it into truffle to build the front-end of the DApp.
+// Remix: https://remix.ethereum.org
+
+// Sign the digital register for the Cryptosoc workshops
+contract WorkshopRegister {
+
+  // a mapping is an efficient data structure that can map every possible
+  // thing to something else - in this case, addresses to booleans
+  mapping(address => bool) private register;
+
+
+  // Add your address as your signature to the register
+  function signRegister() public {
+    address signature = msg.sender;
+    register[signature] = true;
+  }
+
+
+  // Check if you have successfully signed the register
+  function checkRegister(address myAddress) public view returns (bool) {
+    return register[myAddress];
+  }
+}
