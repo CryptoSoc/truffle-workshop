a4f7b5edd41f9637cfd8e5865a80219f078f1e8e
Thomas Tumiel <ttumiel@gmail.com>
2018-10-11 22:21:55 +0200

    truffle migrations

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+// Update your migrations if you change the name of the contracts 
+// or add new ones
+
+var WorkshopRegister = artifacts.require("./WorkshopRegister.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(WorkshopRegister);
+};
