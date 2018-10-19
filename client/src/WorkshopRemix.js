eede8c95be7e879257d9ae5b6c17d62ba3fdd81e
Thomas Tumiel <ttumiel@gmail.com>
2018-10-19 18:22:06 +0200

    remix abi

---
+++ client/src/WorkshopRemix.js
@@ -0,0 +1,64 @@
+const contractABI = [
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "signRegister",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "myAddress",
+				"type": "address"
+			}
+		],
+		"name": "checkRegister",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "signRegisterPayed",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "costInEther",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "_attendee",
+				"type": "address"
+			}
+		],
+		"name": "Signed",
+		"type": "event"
+	}
+]
+
+export default contractABI;
