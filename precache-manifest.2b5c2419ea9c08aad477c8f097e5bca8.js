09cce6c287a485af17bd46f942f2f848a655d9bc
Thomas Tumiel <ttumiel@gmail.com>
2019-01-23 20:26:23 +0200

    Updates

---
+++ precache-manifest.2b5c2419ea9c08aad477c8f097e5bca8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/truffle-workshop/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "69398d30d1067c2cae84",
+    "url": "/truffle-workshop/static/css/main.a113df0b.chunk.css"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/truffle-workshop/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "daa950c8a467fe3d33ad",
+    "url": "/truffle-workshop/static/js/1.daa950c8.chunk.js"
+  },
+  {
+    "revision": "dca21ebe31910ae9a17e",
+    "url": "/truffle-workshop/static/js/runtime~main.dca21ebe.js"
+  },
+  {
+    "revision": "69398d30d1067c2cae84",
+    "url": "/truffle-workshop/static/js/main.69398d30.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/truffle-workshop/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/truffle-workshop/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/truffle-workshop/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "daa950c8a467fe3d33ad",
+    "url": "/truffle-workshop/static/css/1.a2c9a3a0.chunk.css"
+  },
+  {
+    "revision": "2a7646d8c749aea2b8daa4a41e3510a7",
+    "url": "/truffle-workshop/index.html"
+  }
+];
